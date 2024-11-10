386b62ede9512e5853273221f97192fc234797bb
Izabella Aro <izabella.aro20@gmail.com>
2024-11-09 21:22:23 -0300

    fix: remove log da service de pedidos

--- src/services/OrderService.js
+++ src/services/OrderService.js
@@ -4,7 +4,6 @@ export default {
   async fetchOrders() {
     try {
       const response = await urbanGreenAxios.get('/Pedido?skip=0&take=10');
-      console.log(response.data);
       return response.data;
     } catch (error) {
       console.error('Erro ao buscar pedidos:', error);
