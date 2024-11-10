c203eeb0a64dcc3fd8e94b29f3b3d4292c45a97a
Izabella Aro <izabella.aro20@gmail.com>
2024-11-09 21:21:18 -0300

    feat: Extrai header de pagina em componente separado

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="header">
+    <div class="header-title">
+      <h1>{{ title }}</h1>
+    </div>
+
+    <div class="header-actions">
+      <div v-if="addFinder" class="search-container">
+        <input type="text" placeholder="Buscar pedidos..." />
+        <i class="search-icon fa-solid fa-magnifying-glass"></i>
+      </div>
+
+      <i class="more-icon fa-solid fa-plus"></i>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PageHeader',
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    addFinder: {
+      type: Boolean,
+      require: false,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px;
+}
+
+.header-title h1 {
+  margin: 0;
+  font-size: 24px;
+  color: var(--secondary-color);
+}
+
+.header-actions {
+  display: flex;
+  align-items: center;
+}
+
+.search-container {
+  display: flex;
+  align-items: center;
+  border: 1px solid var(--medium-grey-color);
+  border-radius: var(--border-radius);
+  padding-left: 5px;
+  margin-right: 15px;
+}
+
+.search-container input {
+  border: none;
+  outline: none;
+  font-size: 14px;
+  width: 200px;
+}
+
+.search-icon {
+  margin-left: 10px;
+  font-size: 18px;
+  color: white;
+  background-color: var(--primary-color);
+  padding: 10px;
+  border-radius: var(--border-radius);
+}
+
+.more-icon {
+  font-size: 24px;
+  color: white;
+  cursor: pointer;
+  background-color: var(--primary-color);
+  padding: 8px;
+  border-radius: var(--border-radius);
+}
+</style>
--- src/views/OrdersPage.vue
+++ src/views/OrdersPage.vue
@@ -1,19 +1,6 @@
 <template>
   <div class="pedido-view">
-    <div class="header">
-      <div class="header-title">
-        <h1>Pedidos</h1>
-      </div>
-
-      <div class="header-actions">
-        <div class="search-container">
-          <input type="text" placeholder="Buscar pedidos..." />
-          <i class="search-icon fa-solid fa-magnifying-glass"></i>
-        </div>
-
-        <i class="more-icon fa-solid fa-plus"></i>
-      </div>
-    </div>
+    <PageHeader :title="'Pedidos'" :addFinder="true"></PageHeader>
 
     <div class="orders">
       <OrderCard v-for="(order, index) in orders" :key="index" :order="order" />
@@ -24,11 +11,13 @@
 <script>
 import OrderCard from '@/components/OrderCard.vue';
 import OrderService from '@/services/OrderService';
+import PageHeader from '@/components/PageHeader.vue';
 
 export default {
   name: 'OrdersPage',
   components: {
     OrderCard,
+    PageHeader,
   },
   data() {
     return {
@@ -52,58 +41,6 @@ export default {
 </script>
 
 <style scoped>
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 20px;
-}
-
-.header-title h1 {
-  margin: 0;
-  font-size: 24px;
-  color: var(--secondary-color);
-}
-
-.header-actions {
-  display: flex;
-  align-items: center;
-}
-
-.search-container {
-  display: flex;
-  align-items: center;
-  border: 1px solid var(--medium-grey-color);
-  border-radius: var(--border-radius);
-  padding-left: 5px;
-  margin-right: 15px;
-}
-
-.search-container input {
-  border: none;
-  outline: none;
-  font-size: 14px;
-  width: 200px;
-}
-
-.search-icon {
-  margin-left: 10px;
-  font-size: 18px;
-  color: white;
-  background-color: var(--primary-color);
-  padding: 10px;
-  border-radius: var(--border-radius);
-}
-
-.more-icon {
-  font-size: 24px;
-  color: white;
-  cursor: pointer;
-  background-color: var(--primary-color);
-  padding: 8px;
-  border-radius: var(--border-radius);
-}
-
 .pedido-view {
   padding: 20px;
   background-color: white;
